944ce0846074d78167af76a57336fedb39f7bedb
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2025-11-17 07:14:02 +0100

    feat(p1/selection): add aliasing drills (11-15) for Chinook DB

--- pillar1/selection/drill12.sql
+++ pillar1/selection/drill12.sql
@@ -1,3 +1,18 @@
 /*
 
 Select track name AS track_name, milliseconds AS duration_ms.
+
+0|TrackId|INTEGER|1||1
+1|Name|NVARCHAR(200)|1||0
+2|AlbumId|INTEGER|0||0
+3|MediaTypeId|INTEGER|1||0
+4|GenreId|INTEGER|0||0
+5|Composer|NVARCHAR(220)|0||0
+6|Milliseconds|INTEGER|1||0
+7|Bytes|INTEGER|0||0
+8|UnitPrice|NUMERIC(10,2)|1||0
+
+*/
+
+SELECT Name AS track_name, Milliseconds AS duration_ms
+FROM Track;
