56676b34dfacfb7c3ed39c5b87ce3dd676a33280
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2025-11-18 07:29:15 +0100

    feat(p1/selection): complete drills (26-30) on selection applied

--- pillar1/selection/drill29.sql
+++ pillar1/selection/drill29.sql
@@ -2,5 +2,19 @@
 
 Show track name + calculated seconds (milliseconds/1000).
 
+0|TrackId|INTEGER|1||1
+1|Name|NVARCHAR(200)|1||0
+2|AlbumId|INTEGER|0||0
+3|MediaTypeId|INTEGER|1||0
+4|GenreId|INTEGER|0||0
+5|Composer|NVARCHAR(220)|0||0
+6|Milliseconds|INTEGER|1||0
+7|Bytes|INTEGER|0||0
+8|UnitPrice|NUMERIC(10,2)|1||0
 
 */
+
+
+SELECT Name, 
+       Milliseconds / 1000.0 AS seconds
+FROM Track;
