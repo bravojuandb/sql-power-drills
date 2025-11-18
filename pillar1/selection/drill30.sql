0782c9866e238acd0dd3cc661910b970efd7539a
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2025-11-18 07:29:15 +0100

    feat(p1/selection): complete drills (26-30) on selection applied

--- pillar1/selection/drill30.sql
+++ pillar1/selection/drill30.sql
@@ -2,4 +2,17 @@
 
 Return invoice total + 20% tax (calculated column) AS with_tax.
 
+0|InvoiceId|INTEGER|1||1
+1|CustomerId|INTEGER|1||0
+2|InvoiceDate|DATETIME|1||0
+3|BillingAddress|NVARCHAR(70)|0||0
+4|BillingCity|NVARCHAR(40)|0||0
+5|BillingState|NVARCHAR(40)|0||0
+6|BillingCountry|NVARCHAR(40)|0||0
+7|BillingPostalCode|NVARCHAR(10)|0||0
+8|Total|NUMERIC(10,2)|1||0
+
 */
+
+SELECT ROUND(Total * 1.2, 2) AS with_tax
+FROM Invoice;
