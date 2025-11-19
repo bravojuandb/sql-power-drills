e56f2f2125f84a6aec15eb315ef0eeeb91d194ea
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2025-11-19 06:32:46 +0100

    feat(p1/filtering): complete drills 01-05

--- pillar1/filtering/drill03.sql
+++ pillar1/filtering/drill03.sql
@@ -1,1 +1,12 @@
-/* Order artists alphabetically by name.*/
+/* Order artists alphabetically by name.
+
+
+0|ArtistId|INTEGER|1||1
+1|Name|NVARCHAR(120)|0||0
+
+*/
+
+
+SELECT Name
+FROM Artist
+ORDER BY Name;
