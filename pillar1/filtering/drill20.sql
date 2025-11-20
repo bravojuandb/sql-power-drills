31aca5e7679181031872730a475cad75ffdd4780
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2025-11-20 06:50:25 +0100

    feat(p1/filtering): solve drills 16-20 on LIMIT

--- pillar1/filtering/drill20.sql
+++ pillar1/filtering/drill20.sql
@@ -1,1 +1,24 @@
-/* Show the first 7 employees ordered by hire_date.*/
+/* Show the first 7 employees ordered by hire_date.
+
+0|EmployeeId|INTEGER|1||1
+1|LastName|NVARCHAR(20)|1||0
+2|FirstName|NVARCHAR(20)|1||0
+3|Title|NVARCHAR(30)|0||0
+4|ReportsTo|INTEGER|0||0
+5|BirthDate|DATETIME|0||0
+6|HireDate|DATETIME|0||0
+7|Address|NVARCHAR(70)|0||0
+8|City|NVARCHAR(40)|0||0
+9|State|NVARCHAR(40)|0||0
+10|Country|NVARCHAR(40)|0||0
+11|PostalCode|NVARCHAR(10)|0||0
+12|Phone|NVARCHAR(24)|0||0
+13|Fax|NVARCHAR(24)|0||0
+14|Email|NVARCHAR(60)|0||0
+
+*/
+
+SELECT LastName, HireDate AS hire_date
+FROM Employee 
+ORDER BY HireDate 
+LIMIT 7;
