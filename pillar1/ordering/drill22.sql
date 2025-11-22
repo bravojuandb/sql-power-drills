26ada6399301bf088dd6f6c82b0dbc561c50ac3c
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2025-11-22 13:05:38 +0100

    feat(p1/ordering): solve drills 21-25 on ORDER BY + LIMIT

--- pillar1/ordering/drill22.sql
+++ pillar1/ordering/drill22.sql
@@ -1,1 +1,18 @@
-/* Top 5 most expensive invoices.  */
+/* Top 5 most expensive invoices. 
+
+0|InvoiceId|INTEGER|1||1
+1|CustomerId|INTEGER|1||0
+2|InvoiceDate|DATETIME|1||0
+3|BillingAddress|NVARCHAR(70)|0||0
+4|BillingCity|NVARCHAR(40)|0||0
+5|BillingState|NVARCHAR(40)|0||0
+6|BillingCountry|NVARCHAR(40)|0||0
+7|BillingPostalCode|NVARCHAR(10)|0||0
+8|Total|NUMERIC(10,2)|1||0
+
+ */
+
+SELECT InvoiceId, Total
+FROM Invoice 
+ORDER BY Total DESC
+LIMIT 5;
