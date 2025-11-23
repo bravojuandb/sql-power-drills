fc7c585981e6299d4d2a1e0b780c5100881587c4
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2025-11-23 08:21:07 +0100

    feat(p1/filtering): add solution for prevously added drills

--- pillar1_query_fluency/02_filtering/drill11.sql
+++ pillar1_query_fluency/02_filtering/drill11.sql
@@ -1,1 +1,17 @@
-/* Invoices where total is between 5 and 10. */
+/* Invoices where total is between 5 and 10.
+
+0|InvoiceId|INTEGER|1||1
+1|CustomerId|INTEGER|1||0
+2|InvoiceDate|DATETIME|1||0
+3|BillingAddress|NVARCHAR(70)|0||0
+4|BillingCity|NVARCHAR(40)|0||0
+5|BillingState|NVARCHAR(40)|0||0
+6|BillingCountry|NVARCHAR(40)|0||0
+7|BillingPostalCode|NVARCHAR(10)|0||0
+8|Total|NUMERIC(10,2)|1||0
+
+*/
+
+SELECT InvoiceId 
+FROM Invoice 
+WHERE Total BETWEEN 5 AND 10;
