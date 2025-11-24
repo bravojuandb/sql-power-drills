1be6210dc15d8dd3e3128fb7401452e8e7f2b446
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2025-11-24 06:44:42 +0100

    chore(p1/aggregation): add statements for drills 01-05

---
+++ pillar1_query_fluency/04_aggregations/drill03.sql
@@ -0,0 +1,10 @@
+/* Find de minimum and maximum invoice totals
+
+0|InvoiceLineId|INTEGER|1||1
+1|InvoiceId|INTEGER|1||0
+2|TrackId|INTEGER|1||0
+3|UnitPrice|NUMERIC(10,2)|1||0
+4|Quantity|INTEGER|1||0
+
+ */
+
