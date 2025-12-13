17a8fdd91b653fc0d77d52c68dde53965110009d
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2025-12-13 18:02:43 +0100

    feat(p1/null_handling): add drills 21-25 on filtering + null logic

---
+++ pillar1_query_fluency/06_null_logic/drill25.sql
@@ -0,0 +1,27 @@
+/*
+Employees whose manager IS NOT NULL 
+and title = 'Sales Support Agent'.
+
+0|EmployeeId|INTEGER|1||1
+1|LastName|NVARCHAR(20)|1||0
+2|FirstName|NVARCHAR(20)|1||0
+3|Title|NVARCHAR(30)|0||0
+4|ReportsTo|INTEGER|0||0
+5|BirthDate|DATETIME|0||0
+6|HireDate|DATETIME|0||0
+7|Address|NVARCHAR(70)|0||0
+8|City|NVARCHAR(40)|0||0
+9|State|NVARCHAR(40)|0||0
+10|Country|NVARCHAR(40)|0||0
+11|PostalCode|NVARCHAR(10)|0||0
+12|Phone|NVARCHAR(24)|0||0
+13|Fax|NVARCHAR(24)|0||0
+14|Email|NVARCHAR(60)|0||0
+*/
+
+SELECT 
+    EmployeeId,
+    FirstName
+FROM Employee 
+WHERE Title = 'Sales Support Agent'
+    AND ReportsTo IS NOT NULL;
