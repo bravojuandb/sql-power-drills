67406242dff8ac1b78b91796b092991c5b02b5e4
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2026-03-28 21:49:09 +0100

    feat(p2/cte): add fulfilled orders and revenue per shipper query

--- pillar2_analytical_sql/02_ctes/drill08.sql
+++ pillar2_analytical_sql/02_ctes/drill08.sql
@@ -5,11 +5,28 @@
 -- Tables used: Orders, Order Details, Shippers
 
 
+-- Version 1: 
+-- Use DISTINCT because the join to order_details creates multiple rows per order
+-- orders has one row per order.
+-- order_details has one row per item within an order.
+
+WITH fulfilled_orders AS (
+	SELECT 
+		o.ship_via AS shipper_id,
+		sh.company_name AS shipper_name,
+		o.order_id
+	FROM orders o
+	JOIN shippers sh
+		ON o.ship_via = sh.shipper_id
+	WHERE o.shipped_date IS NOT NULL
+)
 SELECT 
-	o.ship_via AS shipper_id,
-	sh.company_name AS shipper_name,
-	o.order_id
-FROM orders o
-JOIN shippers sh
-    ON o.ship_via = sh.shipper_id
-WHERE o.shipped_date IS NOT NULL;
+	fo.shipper_id,
+	fo.shipper_name,
+	COUNT(DISTINCT fo.order_id) AS fulfilled_orders_count,
+	ROUND(SUM((od.unit_price * od.quantity * ( 1 - od.discount))::numeric), 2) AS fulfilled_revenue
+FROM fulfilled_orders fo
+JOIN order_details od
+	ON fo.order_id = od.order_id
+GROUP BY fo.shipper_id, fo.shipper_name
+ORDER BY fulfilled_revenue DESC;
