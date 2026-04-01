8b4b73efc1d7a4c4061b6a726c5a62185515bc00
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2026-04-01 07:22:46 +0200

    feat(p2/ctes): add category revenue drill. alternative solution. v 2

--- pillar2_analytical_sql/02_ctes/drill10.sql
+++ pillar2_analytical_sql/02_ctes/drill10.sql
@@ -31,3 +31,36 @@ SELECT
 FROM enriched_order_lines
 GROUP BY category_id, category_name
 ORDER BY category_revenue DESC;
+
+
+-- This is an alternative way to answer the business question
+-- No need to join orders table
+
+WITH 
+	product_totals AS (
+		SELECT
+			product_id,
+			SUM(unit_price * quantity * (1 - discount)) AS total_revenue
+		FROM order_details
+		GROUP BY product_id
+	),
+	product_categories AS (
+		SELECT 
+			p.product_id,
+			c.category_id,
+			c.category_name
+		FROM products p
+		JOIN categories c
+			ON c.category_id = p.category_id
+	)
+SELECT 
+	pc.category_id,
+	pc.category_name,
+	SUM(pt.total_revenue) AS category_revenue
+FROM product_totals pt
+JOIN product_categories pc
+	ON pc.product_id = pt.product_id
+GROUP BY pc.category_id, pc.category_name
+ORDER BY category_revenue DESC;
+
+
